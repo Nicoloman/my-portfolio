cbff72244f528c25fd9eaf58a682011e1ee13030
niqloma <nicolomanto@gmail.com>
2024-02-22 11:13:02 -0300

    gang changes

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
-
+ngOnInit(){
+  document.addEventListener("fullscreenchange", function () {
+    var video: any = document.getElementById("video-web");
+    if (document.fullscreenElement === video ) {
+      video.controls = false; // Hide controls when entering fullscreen
+    }
+  });
+  
+}
 }
