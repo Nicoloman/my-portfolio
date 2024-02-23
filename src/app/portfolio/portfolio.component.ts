5156ccd42551ece374275a9e634e996059b30f46
niqloma <nicolomanto@gmail.com>
2024-02-23 07:21:06 -0300

    5am club

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,12 +7,6 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 ngOnInit(){
-  document.addEventListener("fullscreenchange", function () {
-    var video: any = document.getElementById("video-web");
-    if (document.fullscreenElement === video ) {
-      video.controls = false; // Hide controls when entering fullscreen
-    }
-  });
-  
 }
+
 }
